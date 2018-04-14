396c3f79bb40889cd01aaecfb27beb634f7295ac
Nujood Ahmed <nujjod@yahoo.com>
2018-04-14 21:34:21 +0300

    delete folders

--- cart.php
+++ cart.php
@@ -49,19 +49,7 @@ require_once('core/init.php');
 							</td>
 							<td class="column-2">Painting</td>
 							<td class="column-3">$36.00</td>
-							<td class="column-4">
-								<div class="flex-w bo5 of-hidden w-size17">
-									<button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
-										<i class="fs-12 fa fa-minus" aria-hidden="true"></i>
-									</button>
-
-									<input class="size8 m-text18 t-center num-product" type="number" name="num-product1" value="1">
-
-									<button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
-										<i class="fs-12 fa fa-plus" aria-hidden="true"></i>
-									</button>
-								</div>
-							</td>
+							<td class="column-4">1</td>
 							<td class="column-5">$36.00</td>
 						</tr>
 
@@ -73,19 +61,7 @@ require_once('core/init.php');
 							</td>
 							<td class="column-2">Wood Sculpture</td>
 							<td class="column-3">$16.00</td>
-							<td class="column-4">
-								<div class="flex-w bo5 of-hidden w-size17">
-									<button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
-										<i class="fs-12 fa fa-minus" aria-hidden="true"></i>
-									</button>
-
-									<input class="size8 m-text18 t-center num-product" type="number" name="num-product2" value="1">
-
-									<button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
-										<i class="fs-12 fa fa-plus" aria-hidden="true"></i>
-									</button>
-								</div>
-							</td>
+							<td class="column-4">1</td>
 							<td class="column-5">$16.00</td>
 						</tr>
 					</table>
