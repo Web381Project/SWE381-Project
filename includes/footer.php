dfae4f5d287266cfb093b1547dbfe9850ae81f9b
Nujood Ahmed <nujjod@yahoo.com>
2018-04-16 17:59:43 +0300

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,10 @@
  <footer>
 
-     <div class="container">
-       <div class="row">
+       <div>
        <center>
            <p>A lovely gate to the Mecca of fine arts</p>
               <p>BORN IN KSU - 2018</p>
    </center>
                </div>
-           </div> 
      
     </footer>
