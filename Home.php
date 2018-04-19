9b5506e60217ea4b64ee39106c2c4adc612f919b
Nujood Ahmed <nujjod@yahoo.com>
2018-04-19 17:35:28 +0300

    update home page

--- Home.php
+++ Home.php
@@ -1,8 +1,5 @@
 <?php
 require_once('core/init.php');
-$sql ="SELECT * FROM products WHERE featured = 1";
-$featured = $db->query($sql);
-
 ?>
 
 <html lang="en"> <!--Language-->
@@ -42,37 +39,6 @@ $featured = $db->query($sql);
       
     </header>    
  
-<div class="container">
-  <h2>Carousel Example</h2>  
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-
-    <!-- Wrapper for slides -->
-      <?php foreach($featured as $product) { ?>
-    <div class="carousel-inner">
-      <div class="item active">
-        <img src="<?php echo $product['image']; ?>" alt="<?php echo $product['title']; ?>" style="width:100%;">
-      </div>
-        <?php } ?>
-
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
-    
- 
-        </div>
- <!--Footer-->
-<?php include 'includes/footer.php' ?>
-    
-    
         
     </body>
   
