48635241cc6133779f24570e1e444c7de8327582
rubaaa <36593053+rubaaa@users.noreply.github.com>
2018-04-23 02:26:07 +0300

    Update ViewReport.php

--- ViewReport.php
+++ ViewReport.php
@@ -40,7 +40,7 @@ session_start();
 				<div class="wrap-table-shopping-cart bgwhite">
 					<table class="table-shopping-cart">
 <tr class="table-head">
-<td class="column-1" style="padding-left: 440px; text-decoration: underline;"> <?php echo	"Existing Products "?></td>
+<td class="column-1" style="padding-left: 440px; text-decoration: underline;"> <?php echo	"The most selling product "?></td>
 </tr>
 </table>
 </div>        
@@ -51,15 +51,19 @@ session_start();
 
 
 <?php 
-if($_GET['order']=="SALES")
-$orders_list ="SELECT * FROM Products WHERE SALES>'0' ORDER BY SALES DESC";
-else
-$orders_list ="SELECT * FROM Products WHERE refuned=='ture' ";
+
+
+$orders_list ="SELECT * FROM Products WHERE sells>'0' ORDER BY sells DESC";
+
+$orders_list2 ="SELECT * FROM Products WHERE refunded='1' ";
 
 
 $query = mysqli_query($db,$orders_list);
+$query2 = mysqli_query($db,$orders_list2);
+
+//-----------------------------------------------
 if (mysqli_num_rows($query) > 0) {
-while ($row=mysqli_fetch_array($query)) {
+$row=mysqli_fetch_array($query) 
 ?>
 	<div class="cart bgwhite p-t-70 p-b-100" style="margin-bottom: 0%">
 		<div class="container">
@@ -81,11 +85,11 @@ while ($row=mysqli_fetch_array($query)) {
 		<tr class="table-row">
 		<td class="column-1">
 								<div class="cart-img-product b-rad-4 o-f-hidden">
-									<img src="images/item-10.jpg" alt="IMG-PRODUCT">
+									<img src="<?php echo $row["image"]; ?>" alt="<?php echo $row["titile"]; ?>">
 								</div>
 							</td>
 							<td class="column-2"><?php echo $row["id"]; ?></td>
-							<td class="column-2"><?php echo $row["SALES"]; ?></td>
+							<td class="column-2"><?php echo $row["sells"]; ?></td>
 							<td class="column-4"><?php echo $row["price"]; ?></td>
 						
 						</tr>
@@ -106,7 +110,7 @@ while ($row=mysqli_fetch_array($query)) {
     </div>
    
 		<?php
-								}
+								
 							} else { ?>
 							<div class="cart bgwhite p-t-70 p-b-100" style="margin-bottom: 0%">
 		<div class="container">
@@ -123,10 +127,98 @@ while ($row=mysqli_fetch_array($query)) {
 </div>
         
         	<?php
-								} ?>
-        
-   
+								} 
+		//-------------------------------------------------						
+		//-----------------------------------------------
+		
+		?>
+		
+		
+								<div class="cart bgwhite p-t-70 p-b-100" style="margin-bottom: 0%">
+		<div class="container">
+			<!-- Cart item -->
+			<div class="container-table-cart pos-relative">
+				<div class="wrap-table-shopping-cart bgwhite">
+					<table class="table-shopping-cart">
+<tr class="table-head">
+<td class="column-1" style="padding-left: 440px; text-decoration: underline;"> <?php echo	"Refunded products"?></td>
+</tr>
+</table>
+</div>        
+</div>
+</div>
+       
+
+<?php
+if (mysqli_num_rows($query2) > 0) {
+while($row=mysqli_fetch_array($query2) ){
+?>
+	<div class="cart bgwhite p-t-70 p-b-100" style="margin-bottom: 0%">
+		<div class="container">
+			<!-- Cart item -->
+			<div class="container-table-cart pos-relative">
+				<div class="wrap-table-shopping-cart bgwhite">
+					<table class="table-shopping-cart">
+						<tr class="table-head">
+							<td class="column-1"></td>
+							<th class="column-2">item ID</th>
+							<th class="column-4">Sales</th>
+							<th class="column-4">price</th>
+							
+			
+							
+							
+						</tr>
 						
+		<tr class="table-row">
+		<td class="column-1">
+								<div class="cart-img-product b-rad-4 o-f-hidden">
+									<img src="<?php echo $row["image"]; ?>" alt="<?php echo $row["titile"]; ?>">
+								</div>
+							</td>
+							<td class="column-2"><?php echo $row["id"]; ?></td>
+							<td class="column-2"><?php echo $row["sells"]; ?></td>
+							<td class="column-4"><?php echo $row["price"]; ?></td>
+						
+						</tr>
+			
+					</table>
+                    
+                    
+                    
+				</div>        
+			</div>
+            
+         
+                
+        </div>
+        
+        
+        
+    </div>
+   
+		<?php
+						}		
+							} else { ?>
+							<div class="cart bgwhite p-t-70 p-b-100" style="margin-bottom: 0%">
+		<div class="container">
+			<!-- Cart item -->
+			<div class="container-table-cart pos-relative">
+				<div class="wrap-table-shopping-cart bgwhite">
+					<table class="table-shopping-cart">
+<tr class="table-head">
+<td class="column-1" style="padding-left: 450px;"> <?php echo	"No Products"?></td>
+</tr>
+</table>
+</div>        
+</div>
+</div>
+        
+        	<?php
+								} ?>  						
+								
+								
+								
 						
 
 
