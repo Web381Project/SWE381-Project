1ea2658d330abba52016dd8aad7db1158a9cba4e
rubaaa <36593053+rubaaa@users.noreply.github.com>
2018-04-23 02:35:26 +0300

    Update Return.php

--- Return.php
+++ Return.php
@@ -1,23 +1,19 @@
-<<<<<<< HEAD
-<?php 
-require_once('core/init.php');
-=======
 
 <?php 
 require_once('core/init.php');
 
->>>>>>> 06457a3aab9266e33d5366b1634bacdc9d582f73
 if(isset($_GET['orderNo']))
     $order_No = $_GET['orderNo'];
     $orders_list1 ="SELECT * FROM orderItem WHERE orderNo='$order_No'";
 $query1 = mysqli_query($db,$orders_list1);
-<<<<<<< HEAD
 if (mysqli_num_rows($query1) > 0) {
 while ($row=mysqli_fetch_array($query1)){
 $itemID=$row["itemID"];
 $qun=$row["quantity"];
 $update ="UPDATE `Products` SET quantity=quantity+'$qun' WHERE id='$itemID'";
+$update6 ="UPDATE `Products` SET sells=sells-'$qun' WHERE id='$itemID'";
  mysqli_query($db,$update);
+  mysqli_query($db,$update6);
 $ordersDelete ="Delete FROM orderItem WHERE itemID='$itemID'";
  mysqli_query($db,$ordersDelete);
 }} 
@@ -25,31 +21,5 @@ $ordersDelete ="Delete FROM orders WHERE orderNo='$order_No'";
 $query1 = mysqli_query($db,$ordersDelete);
     header("Location:billing.php");
 ?>
-=======
-
-if (mysqli_num_rows($query1) > 0) {
-while ($row=mysqli_fetch_array($query1)){
-
-$itemID=$row["itemID"];
 
 
-
-$qun=$row["quantity"];
-
-
-$update ="UPDATE `Products` SET quantity=quantity+'$qun' WHERE id='$itemID'";
- mysqli_query($db,$update);
-
-$ordersDelete ="Delete FROM orderItem WHERE itemID='$itemID'";
- mysqli_query($db,$ordersDelete);
-
-}} 
-
-$ordersDelete ="Delete FROM orders WHERE orderNo='$order_No'";
-$query1 = mysqli_query($db,$ordersDelete);
-
-    header("Location:billing.php");
-
-?>
-
->>>>>>> 06457a3aab9266e33d5366b1634bacdc9d582f73
